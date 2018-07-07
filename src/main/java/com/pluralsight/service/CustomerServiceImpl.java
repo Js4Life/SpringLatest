a9e06e492cf29d0619131d5f74d36da9791170ad
Raghavendra m <raghavendram@Raghavendras-MacBook-Pro.local>
2018-07-07 19:54:14 +0530

    initial spring

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,14 +2,30 @@ package com.pluralsight.service;
 import java.util.List;
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRespository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
+//import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRespository customerRespository = new HibernateCustomerRepositoryImpl();
+//    private CustomerRespository customerRespository = new HibernateCustomerRepositoryImpl();
+
+    private CustomerRespository customerRespository;
+
+    public CustomerServiceImpl() {
+
+    }
+
+    public CustomerServiceImpl(CustomerRespository customerRespository) {
+        this.customerRespository = customerRespository;
+    }
 
     @Override
     public List<Customer> findAll() {
         return customerRespository.findAll();
     }
+
+    public void setCustomerRespository(CustomerRespository customerRespository) {
+        this.customerRespository = customerRespository;
+    }
+
+
 }
